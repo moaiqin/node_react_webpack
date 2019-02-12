5cd8e9f211c7b1cadc069a3c9e62ec11ba3c1e4a
莫绍补 <shaobu.mo@weimob.com>
2019-02-12 09:27:24 +0800

    node and react for ejs

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use((req,res,next) => {
+    res.render('index',{
+        data:JSON.stringify({
+            prefix:'moshaobu.com/',
+            prefixAPI:'/api/'
+        })
+    });
+    next();
+});
+// app.use('/', require('./routers/main'));
+app.listen(3000, () => {
+    console.log('服务端启动成功')
+});
+// mongoose.connect('localhost:27017/newPreject',(err) => {
+//     if(err){
+//         console.log('数据库链接异常');
+//         throw Error(err);
+//     }
+//     console.log('数据库链接成功');
+// })
